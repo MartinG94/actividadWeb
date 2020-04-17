b7de73aadb31e313713d004e4e7572a548344403
MartinG94 <guillenmartin94@gmail.com>
2020-04-17 12:21:14 -0300

    Método necesario para mostrar la biografía de un heroe

--- controller/heroesController.js
+++ controller/heroesController.js
@@ -17,6 +17,24 @@ const heroController = {
         const respuesta = `​Hola, mi nombre es ${heroeBuscado.nombre} y soy ${heroeBuscado.profesion}.​`
         res.send(respuesta);
     },
+    mostrarBiografia: (req, res) => {
+        const numeroHeroe = req.params.idHeroe;
+        const parametroOK = req.params.ok;
+        const heroeBuscado = listadoHeroes.find( (unHeroe) => {
+            return unHeroe.id == numeroHeroe;
+        });
+        if( heroeBuscado == undefined ){
+            res.send(`​"No encontramos un héroe con el id ${numeroHeroe} para mostrarte su biografía"​.`)
+            return;
+        };
+        if( parametroOK == undefined || parametroOK != 'ok' ){
+            res.send(`${heroeBuscado.nombre}. ​Lamento que no desees saber más de mi :(.`);
+            return;
+        };
+        if( parametroOK == 'ok' ){
+            res.send(`${heroeBuscado.nombre}. ${heroeBuscado.resenia}`);
+        };
+    }
 };
 
 module.exports = heroController;
