00d6b0512e90c35ba94f66b5ebca3da8c59b2a11
MartinG94 <guillenmartin94@gmail.com>
2020-04-16 21:04:18 -0300

    Arreglo de variable

--- controller/heroesController.js
+++ controller/heroesController.js
@@ -3,7 +3,7 @@ const heroes = JSON.parse(fs.readFileSync('./data/heroes.json', 'utf-8'));
 
 const heroController = {
     listarHeroes: (req, res) => {
-        res.send(losHeroes);
+        res.send(heroes);
     },
 };
 
